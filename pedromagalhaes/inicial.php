759e8ffa6022bda30ae4c6a564c57eb65a633890
Jonathan Magalhaes <jonathanmundi@gmail.com>
2016-07-12 14:26:37 -0300

    Projeto Magilla: modificações que o cliente pediu

--- pedromagalhaes/inicial.php
+++ pedromagalhaes/inicial.php
@@ -7,22 +7,50 @@ Template Name: inicial
 <?php get_header(); ?>
       
   <div id="image-background"></div>
-  <div class="container pagina-inicial">
-    <div class="box-name text-center wow fadeInDown" data-wow-duration="3s" data-wow-delay="0.5s">
-      <h1>Magilla</h1>
-      <h6>Violinista</h6>
+  	<div class="container pagina-inicial">
+	    <div class="box-name text-center wow fadeInDown" data-wow-duration="3s" data-wow-delay="0.5s">
+	      <h1>Magilla</h1>
+	      <h6>Violinista</h6>
+	    </div>
+    	<!-- FIM DE BOX-NAME -->
+	    <!--<div class="paper">
+	      <h1>Tenha um show único com o melhor da música</h1><br>
+	      <p>Músico atuante no Rio de Janeiro,Magilla oferece música ao vivo em diversos estilos e formações para serviços como pocket shows,música ao vivo em estabelecimentos,casamentos,<br>festas e eventos corporativos.</p>
+	       <a href="http://magillamusic.com.br/videos/" class="btn-papper btn-3">Assista aos Vídeos</a> 
+	       <a href="http://magillamusic.com.br/contato/" class="btn-papper btn-3">Entre em Contato</a> 
+	    </div>
+	    <!-- FIM DE PAPER -->
+		<div class="agenda">
+			<div class="title-evento"><h2>EVENTOS</h2></div>
+			<div class="container bloco-agenda">
+				<div class="container col-md-2 bloco-data ">
+					<div class="data-agenda text-center">
+						<h2>JUL<br>28</h2>
+					</div>
+				</div>
+				<div class="text-agenda col-md-4">Evento: Show com Magilla Jazz Ensemble<br>Local: Armazém 102. Rua Arnaldo Quintela 102, Botafogo<br>Horário: 19:00</div>
+				<div class="container col-md-2 bloco-data">
+					<div class="data-agenda text-center">
+						<h2>JUL<br>21</h2>
+					</div>
+				</div>
+				<div class="text-agenda col-md-4">Evento: Show com Magilla Jazz Ensemble<br>Local: Manifesto BCA, Leme<br>Horário: 21:00</div>
+			</div>
+			<div class="container bloco-agenda">
+				<div class="container col-md-2 bloco-data">
+					<div class="data-agenda text-center">
+						<h2>JUL<br>07</h2>
+					</div>
+				</div>
+				<div class="text-agenda col-md-4">Evento: Show com Magilla Jazz Ensemble<br>Local: Armazém 102. Rua Arnaldo Quintela 102, Botafogo<br>Horário: 19:00</div>
+				<div class="container col-md-2 bloco-data">
+					<div class="data-agenda text-center">
+						<h2>JUL<br>07</h2>
+					</div>
+				</div>
+				<div class="text-agenda col-md-4">Evento: Show com Magilla Jazz Ensemble<br>Local: Armazém 102. Rua Arnaldo Quintela 102, Botafogo<br>Horário: 19:00</div>
+			</div>
+		</div>
     </div>
-    <!-- FIM DE BOX-NAME -->
-    <div class="paper">
-      <h1>Tenha um show único com o melhor da música</h1><br>
-      <p>Músico atuante no Rio de Janeiro,Magilla oferece música ao vivo em diversos estilos e formações para serviços como pocket shows,música ao vivo em estabelecimentos,casamentos,<br>festas e eventos corporativos.</p>
-       <a href="http://magillamusic.com.br/videos/" class="btn-papper btn-3">Assista aos Vídeos</a> 
-       <a href="http://magillamusic.com.br/contato/" class="btn-papper btn-3">Entre em Contato</a> 
-    </div>
-    <!-- FIM DE PAPER -->
-    <!--<div class="responsive-iframe-container  wow fadeIn" data-wow-duration="3s" data-wow-delay="1s">
-      <iframe src="https://calendar.google.com/calendar/embed?title=Eventos&amp;height=400&amp;wkst=1&amp;hl=pt_BR&amp;bgcolor=%23333333&amp;src=lkj71r15uvd0h5elfa290ci7j4%40group.calendar.google.com&amp;color=%23865A5A&amp;ctz=America%2FSao_Paulo" style="border:solid 3px #777" width="58%" height="400" frameBorder="0" scrolling="no"></iframe>
-    </div> FIM DE CONNTAINER PAGINA-INICIAL -->
-  </div>
-  <!--FIM DE RESPONSIVE-IFRAME-CONTAINER -->
+    <!--FIM DE PAGINA-INICIAL -->
 <?php get_footer(); ?>
